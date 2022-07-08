a694b010aaa846b1ed9494a23969c2f7e495cb4c
Joe Zemmels <jzemmels@iastate.edu>
2022-07-07 20:42:01 -0600

    Map height and width are controllable

--- app.R
+++ app.R
@@ -8,7 +8,7 @@
 #
 
 library(shiny)
-library(shinyMobile)
+# library(shinyMobile)
 
 library(stringr)
 library(magrittr)
@@ -17,55 +17,48 @@ library(rvest)
 
 library(leaflet)
 
-# Define UI for application that draws a histogram
-ui <- f7Page(title = "RllTrails",
-             navbar = f7Navbar(title = "Create map of a custom AllTrails List",
-                               hairline = TRUE,
-                               shadow = TRUE),
-             # toolbar = f7Toolbar(position = "bottom",f7Link(label = "AllTrails.com",href = "www.alltrails.com")),
-             f7Shadow(intensit = 16,
-                      hover = TRUE,
-                      f7Card(title = "Create a map of an AllTrails List",
-                             h5('1. Find an AllTrails List that you like'),
-                             h5('2. Click "Share Link" (top-right corner on mobile)'),
-                             h5('3. Copy the link to your clipboard'),
-                             h5('4. Paste the link in the text box below'),
-                             f7Text(inputId = "allTrailsLink","AllTrails link:",value = "https://www.alltrails.com/lists/castle-rock-7634ea6?u=i"),
-                             br(),
-                             tags$div(
-                               leafletOutput(outputId = "allTrailsMap",height = "600",width = "100%")
-                               # htmlOutput(outputId = "allTrailsMap")
-                             ))),
-             options = list(theme = "ios",
-                            dark = TRUE),
-             allowPWA = TRUE)
-
-
-#   fluidPage(
-#
-#     # Application title
-#     titlePanel("All Trails List Map"),
-#
-#     # Sidebar with a slider input for number of bins
-#     sidebarLayout(
-#         sidebarPanel(
-#           textInput(inputId = "allTrailsLink",label = "All Trails Link:",value = "https://www.alltrails.com/lists/castle-rock-7634ea6?u=i")
-#         ),
-#         # Show a plot of the generated distribution
-#         mainPanel(
-#           tags$div(
-#             htmlOutput(outputId = "allTrailsMap")
-#           ),
-#         )
-#     )
-# )
+ui <- shiny::fluidPage(title = "Create a map of an AllTrails List",
+                       column(width = 12,
+                              h3(strong("Create a map of an AllTrails List")),
+                              h4('1. Find an AllTrails List that you like'),
+                              h4('2. Click "Share Link" (top-right corner on mobile)'),
+                              h4('3. Copy the link to your clipboard'),
+                              h4('4. Paste the link in the text box below'),
+                              # br(),
+                              div(style = "display:inline-block;",
+                                  textInput(inputId = "allTrailsLink",
+                                            label = "AllTrails link:",
+                                            width = "300px",
+                                            value = "https://www.alltrails.com/lists/castle-rock-7634ea6?u=i")),
+                              div(style = "display:inline-block;",
+                                  numericInput(inputId = "mapHeight",
+                                               label = "Map Height (pixels)",
+                                               width = "90px",
+                                               value = 800)),
+                              div(style = "display:inline-block;",
+                                  numericInput(inputId = "mapWidth",
+                                               label = "Map Width (% of screen)",
+                                               width = "90px",
+                                               value = 100)),
+                              tags$div(
+                                tags$style("overflow-y: scroll;"),
+                                uiOutput(outputId = "leafletMap")
+                              ))
+)
 
 # Define server logic required to draw a histogram
 server <- function(session,input, output) {
 
+  output$leafletMap <- renderUI({
+
+    leafletOutput(outputId = "allTrailsMap",
+                  height = paste0(input$mapHeight,"px"),
+                  width = paste0(input$mapWidth,"%"))
+
+  })
+
   output$allTrailsMap <-
     renderLeaflet({
-      # renderUI({
 
       req(input$allTrailsLink)
 
@@ -94,7 +87,7 @@ server <- function(session,input, output) {
         mutate(difficultyName = ifelse(difficulty_rating == 1,"Easy",
                                        ifelse(difficulty_rating == 3,"Moderate",
                                               "Hard")),
-        popup = paste0('
+               popup = paste0('
         <img src = "',profile_photo_url,'" width = "150"/>
         <br/><strong>',name,'</strong>, ',route_type,' route
         <br/>',round(duration_minutes/60,2),' hours, ',difficultyName,'
@@ -102,16 +95,12 @@ server <- function(session,input, output) {
         <br/>
         <a target="_blank" rel="noopener noreferrer" href="https://www.alltrails.com/',slug,'"> AllTrails Link </a>'))
 
-      plt <- leaflet() %>%
+      plt <- leaflet(height = input$mapHeight) %>%
         addTiles() %>%
         setView(lng = mean(c(min(dat1$lng),max(dat1$lng))),lat = mean(c(min(dat1$lat),max(dat1$lat))),zoom = 10) %>%
         addCircleMarkers(lng = dat1$lng,lat = dat1$lat,popup = dat1$popup)
 
       return(plt)
-
-      # return(HTML(paste0('
-      #            <iframe class="alltrails" src="',link,'" width="100%" height="800" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="AllTrails: Trail Guides and Maps for Hiking, Camping, and Running"></iframe>
-      #            ')))
 
     })
 
